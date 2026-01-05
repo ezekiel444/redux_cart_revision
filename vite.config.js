ad8f22ca6eaac1ddd13cae5c69e7178ad9b83e22
matomitw <matomi-lucky.ezekiel@teamwork.net>
2026-01-05 11:54:28 +0100

    fixes MIME type issues

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined  // Fixes MIME type issues
+      }
+    },
+  },
   server: {
     port: 3000,
     open: true
   }
-});
+})
